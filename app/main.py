ac34f08c0cbe093dca3af7e89afc677498848771
Thomas.Pye <thomaspye123@live.com>
2026-09-05 23:18:15 +0100

    Add alembic database migrations

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
 
 from app.api.exercises import router as exercises_router
-from app.db.database import Base, engine
-from app.models import Exercise
-
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
