4537f18d3791ef9141fe54821a642a80d571ded7
Thomas.Pye <thomaspye123@live.com>
2026-09-10 23:35:04 +0100

    fixed trailing slash for users get

--- app/static/js/api.js
+++ app/static/js/api.js
@@ -2,7 +2,7 @@
 // users
 
 async function getUsers() {
-    const response = await fetch("/api/users");
+    const response = await fetch("/api/users/");
 
     if (!response.ok) {
         throw new Error("Failed to get users");
